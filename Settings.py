86298b635e7267a0cadae3efe9c293e096593d80
anantha krishnan krishnamoorthy <ananthkrish2002@outlook.com>
2021-11-16 16:13:57 +0800

    Refactoring the code

---
+++ Settings.py
@@ -0,0 +1,3 @@
+startDate = '2021-11-01'
+currentDate = '2021-11-07'
+SiteUrl = 'http://122.15.179.102/ARS/home/rainfall/'
--- Webscrapping.py
+++ Webscrapping.py
@@ -1,36 +1,39 @@
+from pandas.core.indexes.datetimes import date_range
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from requests.models import ChunkedEncodingError
+import Settings as st
 
-startDate = '2021-01-01'
-currentDate = '2021-11-07'
-rainfallDate = '2021-11-07'
+date_range = pd.date_range(st.startDate, st.currentDate)
 
+#ghp_142WAP6ifjwTMD4VPNYr8LIkKtH94G7uh2sw7PL
+def scrapRainfallData():
+    for datevalue in date_range:
+        formattedDate = datevalue.strftime('%Y-%m-%d')
+        Url =  f'{st.SiteUrl}{formattedDate}'
+        print(f"Retreiveing datat for {formattedDate}")
+        webScrapping(Url)
+        
 
-# Problem 1 - Write a function to loop between startDate and currentDate and pass the value to below url
-URL =  f'http://122.15.179.102/ARS/home/rainfall/{rainfallDate}'
-r = requests.get(URL)
-  
+def webScrapping(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser') # If this line causes an error, run 'pip install html5lib' or install html5lib
+    data_iterator = iter(soup.find_all('td'))
+    while True:
+             try:
+                 Location = next(data_iterator).text
+                 ActualMM = next(data_iterator).text
+                 NormalMM = next(data_iterator).text
+                 next(data_iterator).text
+                 next(data_iterator).text
+                 next(data_iterator).text
+                 next(data_iterator).text
+                 print(f'{Location} : {ActualMM} : {NormalMM}')
+             except Exception as err:
+                 break          
+     
 
-# Problem 2 - Create function to handle the webscrapping
-# Problem 3 - Call the function in Problem 1 function
-soup = BeautifulSoup(r.content, 'html.parser') # If this line causes an error, run 'pip install html5lib' or install html5lib
-data = []
- 
-
-data_iterator = iter(soup.find_all('td'))
-while True:
-            try:
-                Location = next(data_iterator).text
-                ActualMM = next(data_iterator).text
-                NormalMM = next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                next(data_iterator).text
-                print(f'{Location} : {ActualMM} : {NormalMM}')
-            except Exception as err:
-                print(err)            
-
+scrapRainfallData()
 # Problem 4 - Inject the data in MongoDB
     
